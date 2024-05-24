515df76f918e91d0b57619308f55e2ec5c1fd17a
Ariff Azmi <108802147+AriffAzmi03@users.noreply.github.com>
2024-05-24 23:33:09 +0800

    update

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -24,7 +24,7 @@ include('includes/header.php');
         <!-- Warning Card -->
         <div class="col-xl-3 col-md-6">
             <div class="card bg-warning text-white mb-4">
-                <div class="card-body">Warning Card</div>
+                <div class="card-body">Vehicle Registered</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="#">View Details</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
